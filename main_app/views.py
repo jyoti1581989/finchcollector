67da3e2bb12beeb72ee4653e64b04b023f8f879d
Jyoti Vaishnav <jyoti.vaishnav666@gmail.com>
2023-09-25 16:52:35 -0400

    create finch model and admin portal

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
+# Import the Finch Model
+from .models import Finch
 
-finches = [
-  {'name': 'Bells', 'species': 'crossbills', 'description': ' a large head, males are a distinctive brick-red ', 'age': 3},
-  {'name': 'Goldy', 'species': 'goldfinch', 'description': 'highly coloured finch with a bright red face and yellow wing patch.', 'age': 2},
-]
+
 
 # Create your views here.
 def home(request):
@@ -13,7 +12,12 @@ def about(request):
     return render(request, 'about.html')
  
 def finches_index(request):
+  finches = Finch.objects.all()
   # We pass data to a template very much like we did in Express!
   return render(request, 'finches/index.html', {
     'finches': finches
   })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
