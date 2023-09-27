49d3c011b65582e709d18c9d5c1a08f5abe9b560
Jyoti Vaishnav <jyoti.vaishnav666@gmail.com>
2023-09-27 00:20:10 -0400

    Add feefing model to database

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Import the Finch Model
 from .models import Finch
+from .forms import FeedingForm
 
 
-
-# Create your views here.
+# Create views here.
 def home(request):
     return render(request, 'home.html')
 
@@ -21,7 +21,20 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch })
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', { 
+     'finch': finch, 'feeding_form': feeding_form
+  })
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
    model = Finch
@@ -29,7 +42,7 @@ class FinchCreate(CreateView):
   
 class FinchUpdate(UpdateView):
   model = Finch
-  # Let's disallow the renaming of a cat by excluding the name field!
+  # Let's disallow the renaming of a finch by excluding the name field!
   fields = ['species', 'description', 'age']
 
 class FinchDelete(DeleteView):
